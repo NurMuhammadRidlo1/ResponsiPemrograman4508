5f7b9c2da33693d7bbf69548a5383e75f5268569
NurMuhammadRidlo1 <nurmuhammadridlo@gmail.com>
2022-04-21 09:36:14 +0700

    first commit

---
+++ ResponsiPemrograman4508/Karyawan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4508
+{
+    internal class Karyawan
+    {
+        public string Nik{get; set;}
+        public string Nama { get; set; }
+        public float GajiBulanan { get; set; }
+
+        public Karyawan (string Nik, string Nama, float GajiBulanan)
+        {
+            this.Nik = Nik;
+            this.Nama = Nama;
+            if(GajiBulanan > 0)
+            {
+                this.GajiBulanan = GajiBulanan;
+            } else 
+            {
+                this.GajiBulanan = 0;
+            }
+        }
+
+    }
+}
---
+++ ResponsiPemrograman4508/Program.cs
@@ -0,0 +1,28 @@
+﻿using ResponsiPemrograman4508;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan paijo = new Karyawan("1234", "Paijo", 1000000);
+            Karyawan jono = new Karyawan("5678", "Jono", 2000000);
+
+            Console.WriteLine("No  Nik/Nama           GajiBulanan");
+            Console.WriteLine("==================================");
+            Console.WriteLine("1. " + paijo.Nik+ "/" + paijo.Nama + "           "+ paijo.GajiBulanan);
+            Console.WriteLine("2. " + jono.Nik + "/" + jono.Nama + "            " + jono.GajiBulanan);
+
+            Console.WriteLine("");
+            Console.WriteLine("Kenaikan Gaji 10%");
+            Console.WriteLine("No  Nik/Nama           GajiBulanan");
+            Console.WriteLine("==================================");
+            paijo.GajiBulanan = (float)(paijo.GajiBulanan + (0.1 * paijo.GajiBulanan));
+            Console.WriteLine("1. " + paijo.Nik + "/" + paijo.Nama + "           " + paijo.GajiBulanan);
+            jono.GajiBulanan = (float)(jono.GajiBulanan + (0.1 * jono.GajiBulanan));
+            Console.WriteLine("2. " + jono.Nik + "/" + jono.Nama + "            " + jono.GajiBulanan);
+        }
+    }
+}
